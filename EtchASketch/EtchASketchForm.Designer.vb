a55e465c57b123129e10d42b6e0afb497e029c3d
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-04-05 08:36:08 -0600

    Got the drawing fully fixed and added the show color dialog option

--- EtchASketch/EtchASketchForm.Designer.vb
+++ EtchASketch/EtchASketchForm.Designer.vb
@@ -22,17 +22,166 @@ Partial Class EtchASketchForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PBDrawing = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip2 = New System.Windows.Forms.MenuStrip()
+        Me.FIleToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SelectColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DrawWaveformToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SelectColorButton = New System.Windows.Forms.Button()
+        Me.DrawWaveformButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        CType(Me.PBDrawing, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip2.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'PBDrawing
+        '
+        Me.PBDrawing.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PBDrawing.Location = New System.Drawing.Point(12, 31)
+        Me.PBDrawing.Name = "PBDrawing"
+        Me.PBDrawing.Size = New System.Drawing.Size(996, 479)
+        Me.PBDrawing.TabIndex = 0
+        Me.PBDrawing.TabStop = False
+        '
+        'MenuStrip2
+        '
+        Me.MenuStrip2.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FIleToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem1})
+        Me.MenuStrip2.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip2.Name = "MenuStrip2"
+        Me.MenuStrip2.Size = New System.Drawing.Size(1019, 28)
+        Me.MenuStrip2.TabIndex = 3
+        Me.MenuStrip2.Text = "MenuStrip2"
+        '
+        'FIleToolStripMenuItem
+        '
+        Me.FIleToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.FIleToolStripMenuItem.Name = "FIleToolStripMenuItem"
+        Me.FIleToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FIleToolStripMenuItem.Text = "FIle"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(116, 26)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SelectColorToolStripMenuItem, Me.DrawWaveformToolStripMenuItem, Me.ClearToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(49, 24)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'SelectColorToolStripMenuItem
+        '
+        Me.SelectColorToolStripMenuItem.Name = "SelectColorToolStripMenuItem"
+        Me.SelectColorToolStripMenuItem.Size = New System.Drawing.Size(199, 26)
+        Me.SelectColorToolStripMenuItem.Text = "Select Color"
+        '
+        'DrawWaveformToolStripMenuItem
+        '
+        Me.DrawWaveformToolStripMenuItem.Name = "DrawWaveformToolStripMenuItem"
+        Me.DrawWaveformToolStripMenuItem.Size = New System.Drawing.Size(199, 26)
+        Me.DrawWaveformToolStripMenuItem.Text = "Draw Waveform"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(199, 26)
+        Me.ClearToolStripMenuItem.Text = "Clear"
+        '
+        'HelpToolStripMenuItem1
+        '
+        Me.HelpToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1})
+        Me.HelpToolStripMenuItem1.Name = "HelpToolStripMenuItem1"
+        Me.HelpToolStripMenuItem1.Size = New System.Drawing.Size(55, 24)
+        Me.HelpToolStripMenuItem1.Text = "Help"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(133, 26)
+        Me.ToolStripMenuItem1.Text = "About"
+        '
+        'SelectColorButton
+        '
+        Me.SelectColorButton.Location = New System.Drawing.Point(12, 516)
+        Me.SelectColorButton.Name = "SelectColorButton"
+        Me.SelectColorButton.Size = New System.Drawing.Size(206, 96)
+        Me.SelectColorButton.TabIndex = 4
+        Me.SelectColorButton.Text = "Select Color"
+        Me.SelectColorButton.UseVisualStyleBackColor = True
+        '
+        'DrawWaveformButton
+        '
+        Me.DrawWaveformButton.Location = New System.Drawing.Point(275, 516)
+        Me.DrawWaveformButton.Name = "DrawWaveformButton"
+        Me.DrawWaveformButton.Size = New System.Drawing.Size(206, 96)
+        Me.DrawWaveformButton.TabIndex = 5
+        Me.DrawWaveformButton.Text = "Draw Waveform"
+        Me.DrawWaveformButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(542, 516)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(206, 96)
+        Me.ClearButton.TabIndex = 6
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(802, 516)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(206, 96)
+        Me.ExitButton.TabIndex = 7
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
         '
         'EtchASketchForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1019, 624)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.DrawWaveformButton)
+        Me.Controls.Add(Me.SelectColorButton)
+        Me.Controls.Add(Me.MenuStrip2)
+        Me.Controls.Add(Me.PBDrawing)
         Me.Name = "EtchASketchForm"
         Me.Text = "EtchASketchForm"
+        CType(Me.PBDrawing, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip2.ResumeLayout(False)
+        Me.MenuStrip2.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents PBDrawing As PictureBox
+    Friend WithEvents MenuStrip2 As MenuStrip
+    Friend WithEvents FIleToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SelectColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DrawWaveformToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents SelectColorButton As Button
+    Friend WithEvents DrawWaveformButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
 End Class
--- EtchASketch/EtchASketchForm.vb
+++ EtchASketch/EtchASketchForm.vb
@@ -2,11 +2,75 @@
 'RCET0265
 'Spring 2023
 'Etch-A-Sketch
-'
+'https://github.com/JoshuaMakuch/EtchASketch.git
 
 Option Strict On
 Option Explicit On
 
+Imports System.Drawing
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Window
+Imports Microsoft.VisualBasic.Devices
+
 Public Class EtchASketchForm
+    Dim draw As Boolean
+    Dim g As Graphics = Me.CreateGraphics
+    Dim myPen As Pen = New Pen(Color.Black)
+    Dim bmp As Bitmap
+    Dim mouX As Integer
+    Dim mouY As Integer
+
+    'On Form Load
+    Private Sub EtchASketchForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Populate picture box image property
+        bmp = New Bitmap(PBDrawing.Width, PBDrawing.Height) 'Sets the bitmap to be the size of the picture box
+        PBDrawing.Image = bmp
+    End Sub
+    'Exit Button
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+    'Exit Menu Button
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+    'Sets the color of the drawn line, Color Button
+    Private Sub SelectColorButton_Click(sender As Object, e As EventArgs) Handles SelectColorButton.Click
+        'Opens a window dialog to be able to change the color as desired
+        Dim myDialog As New ColorDialog()
+        If (myDialog.ShowDialog() = Windows.Forms.DialogResult.OK) Then
+            myPen.Color = myDialog.Color
+        End If
+
+    End Sub
+
+
+    'THIS IS THE MAIN DRAWING CODE
+
+    'When the user presses down their mouse INSIDE the picture box
+    Private Sub PBDrawing_MouseDown(sender As Object, e As MouseEventArgs) Handles PBDrawing.MouseDown
+        draw = True
+        'Sets initial mouse coordinates
+        mouX = e.X
+        mouY = e.Y
+    End Sub
+
+    'When the user moves their mouse and the mouse is pressed down INSIDE the picture box
+    Private Sub PBDrawing_MouseMove(sender As Object, e As MouseEventArgs) Handles PBDrawing.MouseMove
+        If draw Then
+            Using g As Graphics = Graphics.FromImage(PBDrawing.Image)
+                g.DrawLine(myPen, mouX, mouY, e.X, e.Y)
+                'Pulls mouse coordinates to ensure a line is created no matter how fast the user moves
+                mouX = e.X
+                mouY = e.Y
+            End Using
+            PBDrawing.Refresh()
+        End If
+    End Sub
+
+    'When the user lets go of their mouse
+    Private Sub PBDrawing_MouseUp(sender As Object, e As MouseEventArgs) Handles PBDrawing.MouseUp
+        draw = False
+    End Sub
 
 End Class
